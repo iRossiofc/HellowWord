921b7aa3f3c6d0f82303670a992098b8d45ee682
Yydois42 <irossiofc>
2020-12-03 23:46:58 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello word!</Text>
+      <StatusBar style="auto" />
+      <Button
+      title="Learn More"
+      color="blue"
+    accessibilityLabel="Learn more about this purple button"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
